5d494cbec0d830ea27fad5f55e6fe5ad07fce3a9
vijayashekarc <vijayashekarc@gmail.com>
2025-12-28 13:13:02 +0530

    changed directory

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG') == 'True'
-ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['advanced-attendance-calculator.vercel.app', '127.0.0.1', 'localhost']
 
 
 # Application definition
